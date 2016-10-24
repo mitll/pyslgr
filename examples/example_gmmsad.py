e779d42e37ff598c6276c454a4c00e6aaf9366e1
William Campbell <wcampbell@ll.mit.edu>
2016-10-24 17:18:50 -0400

    modified example

--- examples/example_gmmsad.py
+++ examples/example_gmmsad.py
@@ -17,18 +17,18 @@ import sys
 def process_gmmsad(fn, feat_config, model_dir, model_files):
     # Load signal
     x = LLSignal()
-    x.load_pcm_wav(fn)
+    x.load_sph(fn, 0)
 
     gs = GMMSAD(feat_config, model_dir, model_files)
     sad_marks = gs.gmmsad(x)
     print 'sad marks : {}'.format(sad_marks)
 
     # JSON config file
-    fp = open('../config/lid_config.json','r')
+    fp = open('config/lid_config.json','r')
     c = json.load(fp)
     fp.close()
 
-    # Feature config for LID
+    # Feature config for extracted features
     feat_config = c['lid_config']['feat_config']
     feat_config['fb_only'] = 'true'
     mfcc_config = json.dumps(feat_config)
@@ -41,19 +41,17 @@ def process_gmmsad(fn, feat_config, model_dir, model_files):
     # Load and apply sad marks
     print 'Number of feature vectors before SAD: {}'.format(f.num_vec())
     f.load_sad_marks(sad_marks)
-    f.save_sad_marks('tmp/wow.mark')
+    f.save_sad_marks('tmp/eg.mark')
     f.apply_sad()
     print 'Number of feature vectors after SAD: {}'.format(f.num_vec())
-    # f.save_sad_marks('tmp/eg_sad.mark')
-
     return
  
 if __name__ == "__main__":
-    slgr_config_fn = '../config/slgr_params.json'
-    model_dir = '../model'
+    sad_config_fn = 'config/gmmsad_params.json'
+    model_dir = '../models'
 
     # JSON config file
-    fp = open(slgr_config_fn,'r')
+    fp = open(sad_config_fn,'r')
     c = json.load(fp)
     fp.close()
 
@@ -65,9 +63,8 @@ if __name__ == "__main__":
     print 'Using models: {}'.format(model_files)
 
     # Processing
-    fn1 = '../slgr_sad/test/0000001.wav'
-    # fn1 = '../slgr_test/test/dlpt-ara-D4m-RZ.wav'
-    print "Processing file: " , fn1
-    process_gmmsad(fn1, feat_config, model_dir, model_files)
+    fn = 'signals/example2.sph'
+    print "Processing file: " , fn
+    process_gmmsad(fn, feat_config, model_dir, model_files)
     
     print 'Done!  Successfully completed GMMSAD tests'
