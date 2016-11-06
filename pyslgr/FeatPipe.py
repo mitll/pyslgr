80fe519413a20a11524cee13d757db479ad3cd0f
William Campbell <wcampbell@ll.mit.edu>
2016-11-06 14:21:18 -0500

    minor changes and lid pipe example

--- pyslgr/FeatPipe.py
+++ pyslgr/FeatPipe.py
@@ -8,8 +8,8 @@ class FeatPipe(object):
     """
     def __init__ (self, config, featClass, sadClass):
        """
-       config : a dictionary of config parameters with two main keys 'feat_config', 'sad_config'.
-       config['feat_config'] has keys:
+       config : a dictionary of config parameters with two main keys 'pipe_config', 'sad_config'.
+       config['pipe_config'] has keys:
            'accel_spread' : int
            'delta_spread' : int
            'delta2point'  : True/False
@@ -26,7 +26,7 @@ class FeatPipe(object):
        featClass : an LLFeatures compatible class
        sadClass  : a class with constructor sadClass(config) and method sadClass.process(LLSignal x, LLFeatures f)
        """
-       self.feat_config = deepcopy(config['feat_config'])
+       self.pipe_config = deepcopy(config['pipe_config'])
        self.feat = featClass
        if sadClass is None:
            self.sad = None
@@ -42,42 +42,39 @@ class FeatPipe(object):
         """
 
         # Basic feature processing
-        f = self.feat(self.feat_config['feat_config'])
+        f = self.feat(self.pipe_config['feat_config'])
         f.process(x)
 
         # Rasta
-        if ('do_rasta' in self.feat_config) and self.feat_config['do_rasta']:
+        if ('do_rasta' in self.pipe_config) and self.pipe_config['do_rasta']:
             f.rasta()
 
         # Various delta style features
-        if ('do_delta' in self.feat_config) and self.feat_config['do_delta']:
-            if ('delta2point' in self.feat_config) and self.feat_config['delta2point']:
-                f.delta2point(self.feat_config['delta_spread'])
+        if ('do_delta' in self.pipe_config) and self.pipe_config['do_delta']:
+            if ('delta2point' in self.pipe_config) and self.pipe_config['delta2point']:
+                f.delta2point(self.pipe_config['delta_spread'])
             else:
-                f.delta(self.feat_config['delta_spread'])
-        if ('do_accel' in self.feat_config) and self.feat_config['do_accel']:
-            f.accel(self.feat_config['accel_spread'])
+                f.delta(self.pipe_config['delta_spread'])
+        if ('do_accel' in self.pipe_config) and self.pipe_config['do_accel']:
+            f.accel(self.pipe_config['accel_spread'])
             feat_str += 'a'
-        if ('do_sdc' in self.feat_config) and self.feat_config['do_sdc']:
-            f.sdc(*self.feat_config['sdc_params'])
+        if ('do_sdc' in self.pipe_config) and self.pipe_config['do_sdc']:
+            f.sdc(*self.pipe_config['sdc_params'])
         
         # Now apply SAD
-        print 'Number of frames is : {}'.format(f.num_vec())
         if self.sad is not None:
             sad_marks = self.sad.process(x, f)  # returns None if applied directly to f
             if sad_marks is not None:
-                print 'sad marks: {}'.format(sad_marks)
                 f.load_sad_marks(sad_marks)
             f.apply_sad()
-        print 'Number of frames is : {}'.format(f.num_vec())
 
         # Now finish off with feat norm
-        if ('do_feat_norm' in self.feat_config) and self.feat_config['do_feat_norm']:
+        if ('do_feat_norm' in self.pipe_config) and self.pipe_config['do_feat_norm']:
             f.feat_norm()
 
         # Set the output features
-        if 'outfeat' in self.feat_config:
-            f.set_outfeat(self.feat_config['outfeat'])
+        if 'outfeat' in self.pipe_config:
+            f.set_outfeat(self.pipe_config['outfeat'])
 
         return f
 
